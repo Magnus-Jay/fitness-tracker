3b788a79e757a852d9a6f1e834151446c6e3b973
Magnus Appel <68828420+magnuss86@users.noreply.github.com>
2020-10-30 09:49:06 -0700

    updating html

--- public/server.js
+++ public/server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitnessdb", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitnessappdb", { useNewUrlParser: true });
 
 // db.workout.create({ name: "legs" })
 //   .then(dbworkout => {
